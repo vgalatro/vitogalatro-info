3242635dd130f7f454ca769c6cd9ac540452a5bd
Vito Galatro <vjgalatro@gmail.com>
2014-05-15 11:37:49 -0400

    Initial commit of node app

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express'),
+  stylus = require('stylus'),
+  nib = require('nib');
+var app = express();
+
+/* Set-up app */
+
+function compile(str, path) {
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware({ 
+  src: __dirname + '/public', 
+  compile: compile
+}));
+app.use(express.static(__dirname + '/public'));
+
+/* Start app */
+app.listen(3000);
+
+app.get('/', function(req, res) {
+  res.render('index.jade', {
+    title : 'Vito Galatro | Web Application Developer'
+  });
+});
+
+app.get('/hello.txt', function(req, res) {
+  res.send('Hello World!');
+});
+
+
